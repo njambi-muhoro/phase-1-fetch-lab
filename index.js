d0595c02363374afec891befc6b7d70bc0bacdf6
+ <+>
2022-11-30 13:43:00 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((res)=>{
+    return res.json();
+  })
+  .then( (json)=> renderBooks(json));
 }
+   
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
